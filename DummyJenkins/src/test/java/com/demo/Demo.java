ae0b81472dbf457ce5020d97e9c4fb7bde20ea73
user <user@DESKTOP-IDBM9JE>
2023-01-07 00:01:57 +0530

    Jenkins Commit

---
+++ DummyJenkins/src/test/java/com/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void m1() {
+		Reporter.log("sanju",true);
+	}
+}
---
+++ DummyJenkins/src/test/java/com/demo/Demo1.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+@Test
+public void m2() {
+	Reporter.log("santhu",true);
+}
+}
